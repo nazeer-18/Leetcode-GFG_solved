c43e448cf933401536742a8dbc20827db3fd91e8
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-01-25 22:59:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (53.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++) {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else if(s[i]==')') {
+                if(!st.empty() && st.top()=='(') st.pop();
+                else st.push(s[i]);
+            }
+            else if (!st.empty() && s[i]=='}') {
+                if(st.top()=='{') st.pop();
+                else st.push(s[i]);
+            }
+            else{
+                if(!st.empty() && st.top()=='[') st.pop();
+                else st.push(s[i]);
+            }
+        }
+                   return st.empty();
+    }
+};
