1385f5defbb07e9dcb2e2024fe67f5ec76b98752
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-01-28 10:34:52 +0530

    Time: 366 ms (53.75%), Space: 148.2 MB (33.78%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int,greater<int>> s;
-        for(int i = 0; i< k; i++){
-            s.insert(nums[i]);
+        int n = nums.size();
+        vector<int>l,r(n);
+        for(int i = 0;i<n;i++){
+            l.push_back(nums[i]);
+            if(i%k!=0){
+                l[i]=max(l[i],l[i-1]);
+            }
+        }
+        r[n-1]=(nums[n-1]);
+        for(int i = n-2;i>=0;i--){
+            r[i]=(nums[i]);
+            if(i%k!= k-1){
+                r[i]=max(r[i],r[i+1]);
+            }
         }
         vector<int>ans;
-        ans.push_back(*s.begin());
-        for(int i = k; i<nums.size(); i++){
-            s.insert(nums[i]);
-            s.erase(s.lower_bound(nums[i-k]));
-            ans.push_back(*s.begin());
+        for(int i = 0; i < n-k+1 ; i++){
+            ans.push_back(max(r[i],l[i+k-1]));
         }
         return ans;
     }
