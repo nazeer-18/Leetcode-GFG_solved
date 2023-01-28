01fcfb61ad38fee6baa15859374a8102d8f8268b
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-01-28 15:07:12 +0530

    Time: 714 ms (23.81%), Space: 134.5 MB (92.78%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int>l,r(n);
-        for(int i = 0;i<n;i++){
-            l.push_back(nums[i]);
-            if(i%k!=0){
-                l[i]=max(l[i],l[i-1]);
-            }
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+    int n = arr.size();
+        deque<int> dq;
+    vector<int> ans;
+    for(int i = 0; i < k; i++) {
+        while(!dq.empty() && arr[dq.back()] < arr[i]){
+            dq.pop_back();
         }
-        r[n-1]=(nums[n-1]);
-        for(int i = n-2;i>=0;i--){
-            r[i]=(nums[i]);
-            if(i%k!= k-1){
-                r[i]=max(r[i],r[i+1]);
-            }
+        dq.push_back(i);
+    }
+    ans.push_back(arr[dq.front()]);
+    for(int i = k; i < n ; i++) {
+        if(dq.front()==i-k) {
+            dq.pop_front();
         }
-        vector<int>ans;
-        for(int i = 0; i < n-k+1 ; i++){
-            ans.push_back(max(r[i],l[i+k-1]));
+        while(!dq.empty() && arr[dq.back()] < arr[i]){
+            dq.pop_back();
         }
+        dq.push_back(i);
+        ans.push_back(arr[dq.front()]);
+    }
         return ans;
     }
 };
