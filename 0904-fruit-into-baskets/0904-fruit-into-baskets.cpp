da87b2920b189876b3e3e8abce828b099a8a2657
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-02-08 00:37:53 +0530

    Time: 122 ms (94.17%), Space: 66.2 MB (89.54%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        if(fruits.size()<=2) return fruits.size();
+        int i = 0, j = 0, first=fruits[0], second=fruits[0], maxLen=0, lastFirst=0, lastSecond=0;
+    while(i<=j && j <fruits.size()) {
+        while(j<fruits.size() && second==first) {
+            j++;
+            second=fruits[j];
+            lastFirst=j-1;
+        }
+        if(j==fruits.size()) {
+             maxLen = max(maxLen,j-i);
+            return maxLen;
+        }
+        if(fruits[j]==first){
+            lastFirst=j;
+        } 
+        else if(fruits[j]==second) {
+            lastSecond = j;
+        }
+        else{
+            if(lastFirst>lastSecond){
+                i = lastSecond+1;
+            }
+            else{
+                i = lastFirst+1;
+                lastFirst = j-1;
+            }
+            lastSecond = j;
+            first = fruits[i];
+            second = fruits[j];
+        }
+         maxLen = max(maxLen,j-i+1);
+        j++;
+    } 
+        return maxLen;
+    }
+};
