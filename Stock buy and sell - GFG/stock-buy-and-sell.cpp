bf1a66dfc28a4cf1b355d184e0842bfffa0408f0
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-02-08 18:23:55 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell - GFG/stock-buy-and-sell.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        vector<vector<int>> ans;
+        int prev=0;
+        for(int i = 1; i< n;i++){
+            if(A[i]<A[i-1]){
+                vector<int> temp;
+                if(A[prev] < A[i-1]){
+                    temp.push_back(prev);
+                    temp.push_back(i-1);
+                    ans.push_back(temp);
+                }
+                prev = i;
+            }
+        }
+        if(A[n-1]>A[prev]){
+             vector<int> temp;
+                temp.push_back(prev);
+                temp.push_back(n-1);
+                ans.push_back(temp);
+        }
+        // for(auto i : ans){
+        //     for(auto j : i) cout<<j<<" ";
+        //     cout<<endl;
+        //}
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(vector<vector<int>> ans, vector<int> A, int p)
+{
+    int c = 0;
+    for(int i=0; i<ans.size(); i++)
+        c += A[ans[i][1]]-A[ans[i][0]];
+    return (c==p) ? 1 : 0;
+}
+
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for (int i=0; i<n; ++i){
+            cin>>A[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.stockBuySell(A, n);
+        int p = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            int x = A[i+1]-A[i];
+            if(x>0)
+                p += x;
+        }
+        if(ans.size()==0)
+            cout<<"No Profit";
+        else{
+            cout<<check(ans,A,p);
+        }cout<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
