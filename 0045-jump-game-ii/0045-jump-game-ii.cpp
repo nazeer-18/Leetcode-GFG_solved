0c74e5cf69960911e016a919159fb8f23cf704a4
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-02-08 17:10:55 +0530

    Time: 16 ms (80.26%), Space: 16.5 MB (84.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int i = 0,ans=0,end=0,maxJmp=0;
+        for(i = 0; i < nums.size()-1; i++) {
+            maxJmp = max(maxJmp,i+nums[i]);
+            if(i==end) {
+                ans++;
+                end = maxJmp;
+            }
+        }
+        return ans;
+    }
+};
