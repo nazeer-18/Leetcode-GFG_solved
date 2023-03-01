7b5f018bb709952523aef931b2d3eb016cff076b
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-03-01 16:32:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (66.51%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string interpret(string s) {
+        string ans;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i]=='G') ans+='G';
+            else if (s[i]=='(' && s[i+1]==')')
+            {
+                ans+='o';
+                i++;
+            }else{
+                ans+="al";
+                i+=3;
+            }
+        }
+        return ans;
+    }
+};
