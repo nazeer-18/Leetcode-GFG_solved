ceb5fbe0daca00c5bc8d84bcbb1df60e4ba7e057
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-03-19 22:50:50 +0530

    Time: 0 ms (100.00%), Space: 10 MB (51.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,11 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p && q && p->val==q->val){
-            if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right)) return true;
-            return false;
-        }
-        if(p==NULL && q==NULL) return true;
-        return false;
+        if(p==NULL || q==NULL) return (p==q);
+        return (p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
