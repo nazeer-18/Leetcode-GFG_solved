32307ef84fefc9092998a4c125fcbbfa0acc1e0f
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-03-21 00:32:09 +0530

    Added solution - LeetHub

---
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       stack<pair<int,int>> st;
+       vector<int> ans;
+       for(int i = 0; i < n; i++) {
+           if(st.empty() || st.top().first> price[i]) {
+               st.push({price[i],1});
+               ans.push_back(1);
+           }else{
+               ans.push_back(1);
+               while(!st.empty() && st.top().first<=price[i]){
+                   ans[ans.size()-1]+= st.top().second;
+                   st.pop();
+               }
+               st.push({price[i],ans[ans.size()-1]});
+           }
+       }
+       return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
