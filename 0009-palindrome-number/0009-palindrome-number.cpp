328e6c9e7301268c641bc8bec58da33c28e3ef24
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-06-20 23:11:55 +0530

    Time: 14 ms (66.39%), Space: 6.1 MB (19.60%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(int x) {
         string s1 = to_string(x),s2 =s1;
         reverse(s1.begin(),s1.end());
-        if(s2==s1) return true;
+           if(s2==s1) return true;
         return false;
     }
 };
