f53edd344d44fa485c9da3c8873067fcfead3889
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-07-29 19:41:54 +0530

    Added solution - LeetHub

---
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template for C++ solution
+
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n){
+        // Your code here
+        stack<long long>st;
+        vector<long long> ans;
+        int i = n-1;
+        for(;i>=0;i--){
+            if(st.empty()){
+                ans.push_back(-1);
+            }else if(st.top()>arr[i]){
+                ans.push_back(st.top());
+            }else{
+                while(!st.empty() && st.top()<=arr[i]){
+                    st.pop();
+                }
+                if(st.empty()) ans.push_back(-1);
+                else ans.push_back(st.top());
+            }
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        
+        vector <long long> res = ob.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
