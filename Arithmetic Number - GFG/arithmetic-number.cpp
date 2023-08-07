c1ebb1a67784af43e12521975d13b67596543722
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-08-07 23:56:13 +0530

    Added solution - LeetHub

---
+++ Arithmetic Number - GFG/arithmetic-number.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int a, int b, int c){
+        // code here
+        //tn = a+'n-1'*c
+        b-=a;
+        if((c==0 && b==0 )|| (c && b/c>0 && b%c==0) || b==0) return 1;
+        return 0;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
