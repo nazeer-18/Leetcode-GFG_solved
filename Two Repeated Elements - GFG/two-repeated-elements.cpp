64f50b5cfe3d14508f873eccd4b72486845512f7
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-12-09 02:38:46 +0530

    Added solution - LeetHub

--- Two Repeated Elements - GFG/two-repeated-elements.cpp
+++ Two Repeated Elements - GFG/two-repeated-elements.cpp
@@ -11,44 +11,19 @@ using namespace std;
 class Solution {
   public:
     //Function to find two repeated elements.
-    vector<int> twoRepeated (int a[], int n) {
+    vector<int> twoRepeated (int arr[], int n) {
         // Your code here
-        vector<int> v;
-        int f=0,s=0,i=0,eor=0;
+        vector<int> ans;
+        int i=0;
         while(i<n+2){
-            eor^=a[i];
-            if(i+1<=n) eor^=(i+1);
-            i++;
-        }
-        int sb = 1<<(int)log2(eor);
-        i=0;
-        //cout<<sb<<endl;
-        while(i<n+2){
-            if(i<n && sb&(i+1)) f^=(i+1);
-            if(sb&a[i]) f^=a[i];
-            i++;
-        }
-        // cout<<f<<endl;
-        // return vector<int> {f,s};
-        if(f==0) f=sb;
-        s=eor^f;i=0;
-        int fcnt=0,scnt=0;
-        while(i<n+2){
-            if(a[i]==f) fcnt++;
-            else if(a[i]==s) scnt++;
-            if(fcnt==2){
-                v.push_back(f);
-                v.push_back(s);
-                return v;
-            }
-            if(scnt==2){
-                v.push_back(s);
-                v.push_back(f);
-                return v;
+            if(arr[abs(arr[i])]>0){
+                arr[abs(arr[i])]*=-1;
+            }else{
+                ans.push_back(abs(arr[i]));
             }
             i++;
         }
-        return v;
+        return ans;
     }
 };
 
