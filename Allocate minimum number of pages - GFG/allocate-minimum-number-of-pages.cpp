f5c9cc220c400cbd92fc78f63688014bf45b0e2a
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2023-12-08 05:44:29 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -12,48 +12,45 @@ class Solution
 {
     public:
     //Function to find minimum number of pages.
-    bool isFeasible(int a[],int n,int m,int &mid){
-        int ns=1,sum=0,i=0;
-        while(i<n){
-            if(a[i]>mid){
-                return false;
-            }else{
-                if(sum+a[i]>mid){
-                    ns++;
-                    sum=a[i];
-                    if(ns>m) return false;
-                }else{
-                    sum+=a[i];
+    bool isFeasible(int a[],int n,int m,int mid){
+    int ns=1,sum=0,i=0;
+    while(i<n){
+        if(a[i]>mid) return false;
+        else{
+            if(sum+a[i]>mid){
+                sum=a[i];
+                ns++;
+                if(ns>m){
+                    return false;
                 }
-            }
-            i++;
-        }
-        return true;
-    }
-    int findPages(int a[], int n, int m) 
-    {
-        //code here 
-        if(m>n) return -1;
-        int start = 0,end=0,i=0,mid=0,ans;
-        while(i<n){
-            end+=a[i];
-            i++;
-        }
-        ans=end;
-        while(start<=end){
-            mid = start+(end-start)/2;
-            //cout<<start<<" "<<end<<" "<<mid<<" "<<ans<<endl;
-            if(isFeasible(a,n,m,mid)){
-                ans=min(ans,mid);
-                end=mid-1;
-                //cout<<"yes "<<ans<<endl; 
             }else{
-                //cout<<"no "<<ans<<endl; 
-                start=mid+1;
+                sum+=a[i];
             }
         }
-        return ans;
+        i++;
     }
+    return true;
+}
+int findPages(int a[],int n,int m){
+    if(m>n) return -1;
+    int start=0,end=0,mid=0,ans=0,i=0;
+    while(i<n){
+        end+=a[i];
+        i++;
+    }
+    ans = end;
+    while(start<=end){
+        mid = start+(end-start)/2;
+        if(isFeasible(a,n,m,mid)){
+            ans=min(ans,mid);
+            end=mid-1;
+        }else{
+            start=mid+1;
+        }
+    }
+
+    return ans;
+}
 };
 
 //{ Driver Code Starts.
